927db77883c264a4d6497c91574a6b8160cdb8e7
Simone <simone.pezzulla@stud.unifi.it>
2021-02-27 16:44:26 +0100

    -created updateCentroids() and assignClusters functions. -the output of last iteration is now the number __INT_MAX__

--- KmeansSequential.cpp
+++ KmeansSequential.cpp
@@ -9,8 +9,62 @@
 
 using namespace std;
 
+void updateCentroids(vector<Point> *points, vector<Point> *centroids, int k){
+    vector<int> numPoints(k, 0); // keep trace of the number of points in each cluster
+    vector<float> sumX(k, 0.0);
+    vector<float> sumY(k, 0.0);
+    vector<float> sumZ(k, 0.0);
+
+    for (auto &point : *points) {
+        int pointCluster = point.getCluster();
+        sumX.at(pointCluster) += point.getX();
+        sumY.at(pointCluster) += point.getY();
+        sumZ.at(pointCluster) += point.getZ();
+        numPoints.at(pointCluster)++;
+    }
+
+    for (int i = 0; i < k; i++){
+        float newX = sumX.at(i) / numPoints.at(i);
+        float newY = sumY.at(i) / numPoints.at(i);
+        float newZ = sumZ.at(i) / numPoints.at(i);
+
+        centroids->at(i).setX(newX);
+        centroids->at(i).setY(newY);
+        centroids->at(i).setZ(newZ);
+    }
+
+}
+
+//returns true if some point changed cluster
+bool assignClusters(vector<Point> *points, vector<Point> *centroids, int k){
+    bool clustersChanged = false;
+    for (auto &point : *points) {
+        point.setClusterDistance(__DBL_MAX__);
+        point.setOldCluster(point.getCluster());
+
+        float clusterDistance = point.getClusterDistance();  // the distance between its actual cluster' centroid
+        int clusterIndex = point.getCluster(); // keep trace of witch cluster the point is
+
+        for (int j = 0; j < k; j++) {
+            float distance = distance3d(centroids->at(j), point);
+            if (distance < clusterDistance) {
+                point.setClusterDistance(distance);
+                clusterDistance = distance;
+                point.setCluster(j);
+                clusterIndex = j;
+            }
+        }
+
+        if (point.getCluster() != point.getOldCluster()) {
+            clustersChanged = true;
+        }
+    }
+
+    return clustersChanged;
+}
+
 void kMeans(vector<Point> *points, int epochsLimit, int k) {
-    // Step 1: Chose k random centroids
+    // Step 1: Create k random centroids
     vector<Point> centroids;
     random_device rd;
     default_random_engine engine(rd());
@@ -22,65 +76,27 @@ void kMeans(vector<Point> *points, int epochsLimit, int k) {
         centroids.push_back(c);
     }
 
-    // repeat step 2 and 3 for a number of times less than epochsLimit or until nothing happens
-
-    for(int ep=0; ep < epochsLimit; ep++) {
-        bool clustersChanged = false;
+    for(int ep = 0 ; ep < epochsLimit; ep++) {
+        if (ep != 0) {
+            writeCsv(points, &centroids, ep, k);
+        }
 
         //Step 2: assign dataPoints to the clusters, based on the distance from its centroids
-
-        vector<int> numPoints(k, 0); // keep trace of the number of points in each cluster
-        vector<float> sumX(k, 0.0);
-        vector<float> sumY(k, 0.0);
-        vector<float> sumZ(k, 0.0);
-
-        // assign points to the clusters
-        for (auto &point : *points) {
-            point.setClusterDistance(__DBL_MAX__);
-            point.setOldCluster(point.getCluster());
-
-            float clusterDistance = point.getClusterDistance();  // the distance between its actual cluster' centroid
-            int clusterIndex = point.getCluster(); // keep trace of witch cluster the point is
-
-            for (int j = 0; j < k; j++) {
-                float distance = distance3d(centroids.at(j), point);
-                if (distance < clusterDistance) {
-                    point.setClusterDistance(distance);
-                    clusterDistance = distance;
-                    point.setCluster(j);
-                    clusterIndex = j;
-                }
-            }
-
-            numPoints.at(clusterIndex) += 1;
-            sumX.at(clusterIndex) += point.getX();
-            sumY.at(clusterIndex) += point.getY();
-            sumZ.at(clusterIndex) += point.getZ();
-
-            if (point.getCluster() != point.getOldCluster()) {
-                clustersChanged = true;
-            }
-        }
-
-        // exit if clusters has not been changed
+        bool clustersChanged = assignClusters(points, &centroids, k);
         if (!clustersChanged) {
-            break;
+            break;          // exit if clusters has not been changed
         }
 
-        writeCsv(points, &centroids, ep, k);
-        writeCsv(points, &centroids, __INT_MAX__, k); //last iteration is also 9001
-        lastEpoch++;
+        //Step 3: update centroids
+        updateCentroids(points, &centroids, k);
 
-        //Step 3: updates centroids
+        lastEpoch = ep;
+    }
 
-        for (int i = 0; i < k; i++) {
-            float newX = sumX.at(i) / numPoints.at(i);
-            float newY = sumY.at(i) / numPoints.at(i);
-            float newZ = sumZ.at(i) / numPoints.at(i);
-            centroids.at(i).setX(newX);
-            centroids.at(i).setY(newY);
-            centroids.at(i).setZ(newZ);
-        }
+
+    writeCsv(points, &centroids, __INT_MAX__, k);
+    if (lastEpoch == epochsLimit){
+        cout << "Maximum number of iterations reached!";
     }
     cout << "iterations = " << lastEpoch << "\n";
 }
